90fe70c71833f5f4f31dceead1820d6eccb67caf
kumsa Mergia <kumsamergia@gmail.com>
2025-04-24 17:37:09 +0300

    correct global css and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
-import {Source_Sans_3 as FontSans } from "next/font/google";
+import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
 
 const fontSans = FontSans({
   variable: "--font-sans",
   subsets: ["latin"],
-  weight: ['200','300','400','500','600','700','800','900']
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
   title: "Aterera ",
-  description: "Clarity from Complexity, Turn your documents into insight, Aterera — Summarize beautifully.",
+  description:
+    "Clarity from Complexity, Turn your documents into insight, Aterera — Summarize beautifully.",
 };
 
 export default function RootLayout({
@@ -20,10 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${fontSans.variable} font-sans antialiased`}
-      >
-        {children}
+      <body className={`${fontSans.variable} font-sans antialiased`}>
+        <div className="relative flex min-h-screen flex-col">
+          <Header />
+          <main className="flex-1"> {children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
