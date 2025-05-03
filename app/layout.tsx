0961c522815fb80209b43006bfb1afca4668e5cf
kumsa Mergia <kumsamergia@gmail.com>
2025-05-03 11:56:21 +0300

    Remove Google Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,53 +1,19 @@
-// import type { Metadata } from "next";
-// import { Source_Sans_3 as FontSans } from "next/font/google";
-// import "./globals.css";
-// import Header from "@/components/common/header";
-// // import Footer from "@/components/common/footer";
-// import { ClerkProvider } from "@clerk/nextjs";
-// import { Toaster } from "@/components/ui/sonner"
-
-// const fontSans = FontSans({
-//   variable: "--font-sans",
-//   subsets: ["latin"],
-//   weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
-// });
-
-// export const metadata: Metadata = {
-//   title: "Aterera ",
-//   description:
-//     "Clarity from Complexity, Turn your documents into insight, Aterera — Summarize beautifully.",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <ClerkProvider>
-//     <html lang="en">
-//       <body className={`${fontSans.variable} font-sans antialiased`}>
-//         <div className="relative flex min-h-screen flex-col">
-//           <Header />
-//           <main className="flex-1"> {children}</main>
-//           {/* <Footer /> */}
-//         </div>
-//         <Toaster />
-//       </body>
-//     </html>
-//     </ClerkProvider>
-//   );
-// }
-
 import type { Metadata } from "next";
+// import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/common/header";
 // import Footer from "@/components/common/footer";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner"
+
+//const fontSans = FontSans({
+  //variable: "--font-sans",
+  //subsets: ["latin"],
+  //weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+//});
 
 export const metadata: Metadata = {
-  title: "Aterera",
+  title: "Aterera ",
   description:
     "Clarity from Complexity, Turn your documents into insight, Aterera — Summarize beautifully.",
 };
@@ -58,17 +24,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
+      {/* <body className={`${fontSans.variable} font-sans antialiased`}> */}
       <body className="font-sans antialiased">
-        <ClerkProvider>
-          <div className="relative flex min-h-screen flex-col">
-            <Header />
-            <main className="flex-1">{children}</main>
-            {/* <Footer /> */}
-          </div>
-          <Toaster />
-        </ClerkProvider>
+
+        <div className="relative flex min-h-screen flex-col">
+          <Header />
+          <main className="flex-1"> {children}</main>
+          {/* <Footer /> */}
+        </div>
+        <Toaster />
       </body>
     </html>
+    </ClerkProvider>
   );
 }
+
